2f28e818f8605d2335b751eb3baaa0f076b0acf3
marciosb@icloud.com <marciosb@icloud.com>
2020-07-18 18:28:25 -0300

    Criação de Bloc Pattern

---
+++ youtube2/lib/bloc/videos_bloc.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:youtube2/api.dart';
+import 'package:youtube2/models/video.dart';
+import 'dart:async';
+
+class VideosBloc implements BlocBase {
+  Api api;
+  List<Video> videos;
+  final StreamController<List<Video>> _videosController =
+      StreamController<List<Video>>();
+  Stream get outVideos => _videosController.stream;
+  final StreamController<String> _searchController = StreamController<String>();
+  Sink get inSearch => _searchController.sink;
+  VideosBloc() {
+    api = Api();
+    _searchController.stream.listen(_search);
+  }
+
+  void _search(String search) async {
+    videos = await api.search(search);
+  }
+
+  @override
+  void dispose() {
+    _videosController.close();
+    _searchController.close();
+  }
+}
